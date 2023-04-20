32e46e6c2b2029a125834a8a51a18bebae749915
kalanamihiranga_aet <kalana.m@aeturnum.com>
2023-04-20 07:44:23 +0530

    init commit

---
+++ discovery-server/src/test/java/com/techwizlanka/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwizlanka.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
