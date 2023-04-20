51bf20a4e80eae11485f13176a3aa2e81fe4798c
kalanamihiranga_aet <kalana.m@aeturnum.com>
2023-04-20 21:47:07 +0530

    added api gatway and eureka server

--- order-service/src/main/java/com/techwizlanka/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/techwizlanka/orderservice/service/OrderService.java
@@ -7,9 +7,11 @@ import com.techwizlanka.orderservice.model.Order;
 import com.techwizlanka.orderservice.model.OrderLineItems;
 import com.techwizlanka.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
@@ -19,10 +21,11 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,13 +41,16 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
 
         boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::getIsInStock);
+
+        log.info(orderLineItems.toString());
+        log.info("Boolean value is : " + String.valueOf(allProductsInStock));
 
         if (allProductsInStock) {
             orderRepository.save(order);
