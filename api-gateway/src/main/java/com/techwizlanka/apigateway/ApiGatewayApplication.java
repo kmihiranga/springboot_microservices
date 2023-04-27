e6990c5f4e6373c610f49eb439c14fb66dfc3ab7
kalanamihiranga-aet <kalana.m@aeturnum.com>
2023-04-27 07:36:22 +0530

    minor changes

--- api-gateway/src/main/java/com/techwizlanka/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/techwizlanka/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package com.techwizlanka.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
